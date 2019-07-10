3fd93eeda7f36671866d5b2e1e2a7889cf408bdc
lupan68 <892210140@qq.com>
2019-07-10 17:18:13 +0800

    项目

---
+++ src/components/tabbar/MemberContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h3>MemberContainer</h3>
+  </div>
+</template>
+
+<script>
+import {Toast} from 'mint-ui';
+export default {
+  created() {
+    Toast('对不起，服务器请求失败')
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
